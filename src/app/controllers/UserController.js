0bc8c698fd12170cf45eaad48bb9f5692faa7725
Flávio Camilo <flaviocamilo@gmail.com>
2020-02-09 04:24:25 -0300

    Add user update
    Signed-off-by: Flávio Camilo <flaviocamilo@gmail.com>

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -10,6 +10,26 @@ class UserController {
 
 		return response.json({ id, name, email, provider });
 	}
+
+	async update(request, response) {
+		const { email, oldPassword } = request.body;
+
+		const user = await User.findByPk(request.userId);
+
+		if (email && email !== user.email) {
+			const userExists = await User.findOne({ where: { email } });
+
+			if (userExists) return response.status(401).json({ error: 'User already exists.' });
+		}
+
+		if (oldPassword && !(await user.checkPassword(oldPassword))) {
+			return response.status(401).json({ error: 'Password does not match.' });
+		}
+
+		const { id, name, provider } = await user.update(request.body);
+
+		return response.json({ id, name, email, provider });
+	}
 }
 
 export default new UserController();
