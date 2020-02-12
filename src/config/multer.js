94d89cc522d089e0f492de13becf3a010133cd32
Flávio Camilo <flaviocamilo@gmail.com>
2020-02-12 03:37:15 -0300

    Add multer config
    Signed-off-by: Flávio Camilo <flaviocamilo@gmail.com>

---
+++ src/config/multer.js
@@ -0,0 +1,16 @@
+import crypto from 'crypto';
+import multer from 'multer';
+import { extname, resolve } from 'path';
+
+export default {
+	storage: multer.diskStorage({
+		destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+		filename: (request, file, callback) => {
+			crypto.randomBytes(16, (error, response) => {
+				if (error) return callback(error);
+
+				return callback(null, response.toString('hex') + extname(file.originalname));
+			});
+		}
+	})
+};
