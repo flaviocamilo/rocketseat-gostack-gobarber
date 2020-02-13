679e948112648f142d2927e4e210bffc604fee1d
Flávio Camilo <flaviocamilo@gmail.com>
2020-02-13 02:59:29 -0300

    Add service providers listing
    Signed-off-by: Flávio Camilo <flaviocamilo@gmail.com>

---
+++ src/app/controllers/ProviderController.js
@@ -0,0 +1,16 @@
+import File from '../models/File';
+import User from '../models/User';
+
+class ProviderController {
+	async index(request, response) {
+		const providers = await User.findAll({
+			where: { provider: true },
+			attributes: ['id', 'name', 'email', 'avatar_id'],
+			include: [{ model: File, as: 'avatar', attributes: ['name', 'path', 'url'] }]
+		});
+
+		return response.json(providers);
+	}
+}
+
+export default new ProviderController();
